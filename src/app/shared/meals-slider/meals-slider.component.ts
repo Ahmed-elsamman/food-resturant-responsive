09468e3737eff4518d3c92899fe10a1d9f446904
Your Name <samman66512@gmail.com>
2025-07-08 17:36:31 +0300

    stander Comp

---
+++ src/app/shared/meals-slider/meals-slider.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meals-slider',
+  standalone: true,
+  imports: [],
+  templateUrl: './meals-slider.component.html',
+  styleUrl: './meals-slider.component.scss'
+})
+export class MealsSliderComponent {
+
+}
